7a8d13d84068a42b51f8b047ba88ab26df5c6c9e
theo-githu <111968864+theo-githu@users.noreply.github.com>
2023-09-02 18:16:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -214,7 +214,7 @@ app.delete('/users/:Username', (req, res) => {
 
 // Get all movies
 
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/movies', (req, res) => {
     Movies.find()
       .then((movies) => {
         res.status(201).json(movies);
